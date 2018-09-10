206579ff2d40c6638edcaddc1efdc6d28ea4dfca
rodolfostark <rodolfostark@ufrn.edu.br>
2018-09-10 16:41:36 -0300

    finalizando a questao19.c.

--- questao19.c
+++ questao19.c
@@ -1,16 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void multiplicarMatrizes(int * a, int nla, int nca, int * b, int nlb, int * c){	
-	int i, j, k;
+void multiplicarMatrizes(int ** a, int nla, int nca, int ** b, int ncb, int ** c){
+    int i, j, k;
+
+    c = (int **)malloc(nla*sizeof(int *));
+    c[0] = (int *)malloc(nla*ncb*sizeof(int));
+
+     for(i=1;i<nla;i++){
+        c[i] = c[i-1]+ncb;
+    }
+
 	for(i=0;i<nla;i++){
-		for(j=0;j<nla;j++){
+		for(j=0;j<ncb;j++){
 			int escalar = 0;
-			for(k=0;k<nla;k++){
+			for(k=0;k<nca;k++){
 			escalar = escalar + a[i][k]*b[k][j];
 			}
 			c[i][j] = escalar;
 		}
 	}
-	
-	
+	imprimirMatriz(c, nla, ncb);
+}
+void imprimirMatriz(int ** a, int nl, int nc){
+    int i, j;
+    for(i=0;i<nl;i++){
+        for(j=0;j<nc;j++){
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+void preencherMatriz(int ** a, int nl, int nc){
+    int i, j;
+    for(i=0;i<nl;i++){
+        for(j=0;j<nc;j++){
+            a[i][j] = rand()%10 + 1;
+        }
+    }
+}
+int main(void){
+    srand(time(NULL));
+    int ** a, ** b, ** c;
+    int i, nl = 2, nc = 2;
+    a = (int **)malloc(nl*sizeof(int *));
+    a[0] = (int *)malloc(nl*nc*sizeof(int));
+
+    for(i=1;i<nl;i++){
+        a[i] = a[i-1]+nc;
+    }
+
+    b = (int **)malloc(nc*sizeof(int *));
+    b[0] = (int *)malloc(nl*nc*sizeof(int));
+
+     for(i=1;i<nc;i++){
+        b[i] = b[i-1]+nl;
+    }
+
+    preencherMatriz(a, nl, nc);
+    preencherMatriz(b, nc, nl);
+
+    imprimirMatriz(a, nl, nc);
+    imprimirMatriz(b, nc, nl);
+
+    multiplicarMatrizes(a, nl, nc, b, nc, c);
 }
